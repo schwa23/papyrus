29708182c3de60ed228f3f6d646c1cd2eeda6ce5
Joshua Dickens <jdickens@jdickens-mbp.local>
2014-02-26 12:08:37 -0800

    simple / rough uidynamics interaction: attachment behavior and snap behavior

--- papyrus/MainViewController.m
+++ papyrus/MainViewController.m
@@ -16,6 +16,11 @@
 @property (nonatomic) CGFloat releaseVelocity;
 @property (nonatomic) BOOL isForegroundHidden;
 
+@property (nonatomic, strong) UIDynamicAnimator *animator;
+@property (nonatomic, strong) UISnapBehavior *snapBehavior;
+@property (nonatomic, strong) UIDynamicItemBehavior *foregroundDynamicBehavior;
+@property (nonatomic, strong) UIAttachmentBehavior *attachmentBehavior;
+
 - (IBAction)foregroundDidPan:(UIPanGestureRecognizer *)sender;
 
 -(void)snapToBoundaries;
@@ -39,7 +44,37 @@
     self.isForegroundHidden = NO;
     self.initialYposition = 0;
     self.releaseVelocity = 0;
+    
+    
+    
+    
+    
     // Do any additional setup after loading the view from its nib.
+}
+
+-(void) viewDidAppear:(BOOL)animated {
+    
+    UIDynamicAnimator *animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
+    self.animator = animator;
+    
+    UIDynamicItemBehavior *foregroundDynamicBehavior = [[UIDynamicItemBehavior alloc] initWithItems:@[self.foregroundView]];
+    
+    foregroundDynamicBehavior.allowsRotation = NO;
+    self.foregroundDynamicBehavior = foregroundDynamicBehavior;
+    [animator addBehavior:foregroundDynamicBehavior];
+    
+    CGPoint fgCenterPoint = CGPointMake(self.foregroundView.center.x, self.foregroundView.center.y);
+    UIOffset attachmentPoint = UIOffsetMake(0, 0);
+    
+    UIAttachmentBehavior *attachmentBehavior = [[UIAttachmentBehavior alloc] initWithItem:self.foregroundView offsetFromCenter:attachmentPoint attachedToAnchor:fgCenterPoint];
+    
+    attachmentBehavior.damping = .5;
+    attachmentBehavior.frequency = 1;
+    attachmentBehavior.length = 2;
+    [animator addBehavior:attachmentBehavior];
+    
+    self.attachmentBehavior = attachmentBehavior;
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -49,49 +84,56 @@
 }
 
 - (IBAction)foregroundDidPan:(UIPanGestureRecognizer *)sender {
-    NSLog(@"Foreground did pan");
+    [self.animator addBehavior:self.attachmentBehavior];
+    [self.animator removeBehavior:self.snapBehavior];
+
+    
     if (sender.state == UIGestureRecognizerStateBegan) {
-        self.initialYposition = self.foregroundView.frame.origin.y;
+        self.initialYposition = self.foregroundView.center.y;
         
     } else if (sender.state == UIGestureRecognizerStateChanged) {
-        CGRect newFrame = self.foregroundView.frame;
-        NSLog(@"translation in view: %f", [sender translationInView:self.view].y);
-        newFrame.origin.y = self.initialYposition + [sender translationInView:self.view].y;
-        self.foregroundView.frame=newFrame;
+        CGPoint newCenter = self.foregroundView.center;
+        newCenter.x = 160;
+        newCenter.y = self.initialYposition + [sender translationInView:self.view].y;
+        
+        [self.attachmentBehavior setAnchorPoint:newCenter];
         
     } else if (sender.state == UIGestureRecognizerStateEnded) {
         NSLog(@"Release velocity: %f ", self.releaseVelocity);
+        self.releaseVelocity = [sender velocityInView:self.view].y;
 
         [self snapToBoundaries];
     }
-    self.releaseVelocity = [sender velocityInView:self.view].y;
+
 
 }
 
 - (void) snapToBoundaries {
-    CGRect currentFrame = self.foregroundView.frame;
-    CGRect newFrame =currentFrame;
-    CGFloat distanceRemaining;
+    
+    
+    CGPoint newOrigin = CGPointMake(160,284);
+    NSLog(@"- -- snap release %f", self.releaseVelocity);
     if (self.releaseVelocity > 0) {
         //dragging UP
         
-        newFrame.origin.y = 528;
-        distanceRemaining =  528 - currentFrame.origin.y;
+        newOrigin.y = 808;
         self.isForegroundHidden = YES;
         
     } else {
-        
-        newFrame.origin.y = 0;
-        distanceRemaining =  currentFrame.origin.y;
         self.isForegroundHidden = NO;
 
     }
-    CGFloat duration = distanceRemaining * (1.1/528);
-    CGFloat springVelocity = abs(self.releaseVelocity) / 1000;
     
-    [UIView animateWithDuration:duration delay:0 usingSpringWithDamping:1 initialSpringVelocity:springVelocity options:0 animations:^{
-        self.foregroundView.frame = newFrame;
-    } completion:nil];
+    
+    UISnapBehavior *snapBehavior = [[UISnapBehavior alloc] initWithItem:self.foregroundView snapToPoint:newOrigin];
+    snapBehavior.damping = .9;
+    //    [self.animator addBehavior:self.foregroundDynamicBehavior];
+
+//    [self.animator removeBehavior:self.attachmentBehavior];
+    [self.animator addBehavior:snapBehavior];
+
+    
+    self.snapBehavior = snapBehavior;
     
     
 }
