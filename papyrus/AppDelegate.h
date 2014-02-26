25853ec67605d9381c0eb82a8bb3f7fbfbeeda0a
Joshua Dickens <jdickens@jdickens-mbp.local>
2014-02-25 22:37:43 -0800

    Initial Commit

---
+++ papyrus/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  papyrus
+//
+//  Created by Joshua Dickens on 2/25/14.
+//  Copyright (c) 2014 Joshua Dickens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ papyrus/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  papyrus
+//
+//  Created by Joshua Dickens on 2/25/14.
+//  Copyright (c) 2014 Joshua Dickens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
