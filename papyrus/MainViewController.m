066da16b77d112368e34af511c4ab7f2d1cb523c
Joshua Dickens <jdickens@jdickens-mbp.local>
2014-03-03 16:40:10 -0800

    checkpoint, in progress on scaling the scroll view (broken)

--- papyrus/MainViewController.m
+++ papyrus/MainViewController.m
@@ -23,12 +23,24 @@
 @property (nonatomic) CGFloat releaseVelocity;
 @property (nonatomic) BOOL isForegroundHidden;
 
+@property (nonatomic) CGFloat initialCardYposition;
+@property (nonatomic) BOOL isCardViewExpanded;
+@property (nonatomic) BOOL isDraggingCardView;
+@property (nonatomic) CGFloat cardsReleaseVelocity;
+
+
 - (IBAction)foregroundDidPan:(UIPanGestureRecognizer *)sender;
+- (IBAction)handleCardsPanGesture:(UIPanGestureRecognizer *)sender;
 
 - (IBAction)handleForegroundTap:(UITapGestureRecognizer *)sender;
 
 
 -(void)snapToBoundaries;
+-(void)snapCardsToBoundaries;
+-(CGFloat) scaleFromYPosition:(CGFloat)yPosition;
+
+- (NSDictionary *) resizedCardViewAndContentViewWithYPosition:(CGFloat)yPosition;
+
 
 @end
 
@@ -50,9 +62,8 @@
     self.initialYposition = 0;
     self.releaseVelocity = 0;
     
-    self.cardScrollView.contentInset = UIEdgeInsetsMake(0, -8, 0, -4);
-//    self.cardScrollView.contentSize = self.cardView.frame.size;
-    self.cardScrollView.contentSize = CGSizeMake(1605, 262);
+    self.cardScrollView.contentInset = UIEdgeInsetsMake(0, -6, 0, -4);
+    self.cardScrollView.contentSize = self.cardView.frame.size;
     
     
     NSLog(@"Content size: %@", NSStringFromCGSize(self.cardScrollView.contentSize) );
@@ -74,7 +85,7 @@
         CGRect newFrame = self.mainView.frame;
 //        NSLog(@"translation in view: %f", [sender translationInView:self.view].y);
         
-        newFrame.origin.y = self.initialYposition + [sender translationInView:self.view].y;
+        newFrame.origin.y = [sender translationInView:self.view].y;
         NSLog(@"NEW Y position in view: %f",  newFrame.origin.y);
         
         self.releaseVelocity = [sender velocityInView:self.view].y;
@@ -93,6 +104,88 @@
 
 }
 
+- (IBAction)handleCardsPanGesture:(UIPanGestureRecognizer *)sender {
+    CGPoint hitPoint = [sender locationInView:self.view];
+    
+    UIView *targetView = [sender.view hitTest:hitPoint withEvent:nil];
+    if (sender.state == UIGestureRecognizerStateBegan) {
+        self.isDraggingCardView = ([targetView isEqual:self.cardView]);
+        
+//        NSLog(@"Card frame original? %@", NSStringFromCGRect(self.cardView.frame));
+
+    }
+
+    if(self.isDraggingCardView) {
+        if (sender.state == UIGestureRecognizerStateBegan) {
+            self.initialCardYposition = self.cardScrollView.frame.origin.y;
+            
+            
+        } else if (sender.state == UIGestureRecognizerStateChanged) {
+           
+            NSDictionary *frameData = [self resizedCardViewAndContentViewWithYPosition:[sender translationInView:self.view].y];
+//            NSLog(@"OutputY %f", [sender translationInView:self.view].y);
+            
+                  CGRect sFrame;
+            [frameData[@"cardScrollViewFrame"] getValue:&sFrame];
+            self.cardScrollView.frame = sFrame;
+            
+            CGRect cFrame;
+            [frameData[@"cardViewFrame"] getValue:&cFrame];
+            self.cardView.frame = cFrame;
+
+            
+            self.cardsReleaseVelocity = [sender velocityInView:self.view].y;
+            
+        } else if (sender.state == UIGestureRecognizerStateEnded) {
+            
+            self.cardScrollView.pagingEnabled = (self.cardScrollView.frame.size.height > 520);
+            [self snapCardsToBoundaries];
+
+        }
+        
+    }
+    
+    if(sender.state == UIGestureRecognizerStateEnded || sender.state == UIGestureRecognizerStateCancelled) {
+        
+        self.isDraggingCardView = NO;
+    }
+    
+}
+
+- (NSDictionary *) resizedCardViewAndContentViewWithYPosition:(CGFloat)yPosition {
+   
+    
+    CGRect newFrame = self.cardScrollView.frame;
+    
+    newFrame.origin.y = self.initialCardYposition + yPosition;
+    newFrame.size.height = self.view.frame.size.height - newFrame.origin.y;
+    self.cardScrollView.frame = newFrame;
+    
+    
+    CGRect cardFrame = self.cardView.frame;
+    CGFloat scale = [self scaleFromYPosition:newFrame.origin.y];
+    
+    
+    self.cardScrollView.backgroundColor = [UIColor colorWithHue:.8 saturation:.8 brightness:.9 alpha:.5];
+    
+    
+//    NSLog(@"Card frame? %@", NSStringFromCGRect(self.cardView.frame));
+    
+    cardFrame.size.width = 1605*scale;
+    cardFrame.size.height = newFrame.size.height;
+//    cardFrame.origin.x = cardFrame.origin.x * scale;
+    
+
+    
+    return @{@"cardViewFrame": [NSValue valueWithCGRect:cardFrame], @"cardScrollViewFrame": [NSValue valueWithCGRect:newFrame]};
+    
+//    self.cardScrollView.contentSize = cardFrame.size;
+//    self.cardScrollView.contentInset = UIEdgeInsetsMake(0, -6 * scale, 0, -4 * scale);
+    
+    
+    
+}
+
 - (IBAction)handleForegroundTap:(UITapGestureRecognizer *)sender {
     
     if (self.isForegroundHidden ){
@@ -107,8 +200,8 @@
     CGRect currentFrame = self.mainView.frame;
     CGRect newFrame =currentFrame;
     CGFloat distanceRemaining;
+
     if (self.releaseVelocity > 0) {
-        NSLog(@"Release Velocity > 0");
         
         newFrame.origin.y = 528;
         distanceRemaining =  528 - currentFrame.origin.y;
@@ -126,14 +219,73 @@
 
     CGFloat springVelocity = abs(self.releaseVelocity) / 500;
     CGFloat duration = MAX(abs(distanceRemaining) * (1.2/528), .35);
-//    CGFloat duration = .35;
 
 
     
-    [UIView animateWithDuration:duration delay:0 usingSpringWithDamping:1.0 initialSpringVelocity:springVelocity options:0 animations:^{
+    [UIView animateWithDuration:duration delay:0 usingSpringWithDamping:1.0 initialSpringVelocity:springVelocity options:UIViewAnimationOptionAllowUserInteraction animations:^{
         self.mainView.frame = newFrame;
     } completion:nil];
     
+    
+}
+
+- (void) snapCardsToBoundaries {
+    CGFloat newY =0;
+    NSLog(@"card release velocity: %f", self.cardsReleaseVelocity);
+
+
+    if (self.cardsReleaseVelocity > 0) {
+        newY = 0;
+        
+//        distanceRemaining =  528 - currentFrame.origin.y;
+        
+        
+        self.isCardViewExpanded = NO;
+        
+    } else if (self.cardsReleaseVelocity < 0 ){
+        
+        newY = -310;
+
+        self.isCardViewExpanded = YES;
+        
+    }
+    
+    
+    
+    NSDictionary *frameData = [self resizedCardViewAndContentViewWithYPosition:newY];
+    
+    CGRect sFrame;
+    [frameData[@"cardScrollViewFrame"] getValue:&sFrame];
+    
+    
+    CGRect cFrame;
+    [frameData[@"cardViewFrame"] getValue:&cFrame];
+    
+    
+    
+    
+    [UIView animateWithDuration:.5 delay:0 usingSpringWithDamping:.9 initialSpringVelocity:.5 options:UIViewAnimationOptionAllowUserInteraction animations:^{
+        self.cardScrollView.frame = sFrame;
+        self.cardView.frame = cFrame;
+    } completion:^(BOOL finished) {
+        self.cardScrollView.contentSize=cFrame.size;
+    }
+];
+   
+    
+    
+}
+
+- (CGFloat) scaleFromYPosition:(CGFloat) yPosition {
+    
+    CGFloat const inMin = 306;
+    CGFloat const inMax = 0;
+    CGFloat const outMin = 1.0;
+    CGFloat const outMax = 1.8562091503;
+    CGFloat in = yPosition;
+    CGFloat out = outMin + (outMax - outMin) * (in - inMin) / (inMax - inMin);
+    
+    return out;
     
 }
 @end
